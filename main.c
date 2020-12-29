997b919f52029cd65db6efa60772b65ad3691ee2
Henrique Ramos <henriqueramos@Henriques-Air.lan>
2020-12-29 22:07:39 +0000

    save

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int	main()
 {
 	char test[90];
 
-	ft_memcpy(test, "ola tudo bem\n", 13);
+	ft_memcpy(test, "ola tudo bem?\n", 14);
 	ft_printf("a %c %X %mano %o se ok\n", 't', 524275016, -12);
 	printf("%X", 524275016);
 	return (0);
